8b628f082e56b544f97589042db315255e140b76
LeeWongSnail <@wangli_0632@163.com>
2016-09-08 16:54:01 +0800

    add iamge

--- ALiProgressHUD/ViewController.m
+++ ALiProgressHUD/ViewController.m
@@ -7,12 +7,54 @@
 //
 
 #import "ViewController.h"
+#import "NSObject+ALiHUD.h"
 
 @interface ViewController ()
-
+@property (nonatomic, assign) CGFloat percent;
+@property (nonatomic, strong) NSTimer *timer;
 @end
 
 @implementation ViewController
+
+- (IBAction)loadingAction:(UIButton *)sender {
+    [self showLoading];
+}
+
+- (IBAction)normalTextAction:(UIButton *)sender {
+    [self showText:sender.currentTitle];
+}
+
+- (IBAction)failureAction:(UIButton *)sender {
+    [self showErrorText:sender.currentTitle];
+}
+
+- (IBAction)successAction:(UIButton *)sender {
+    [self showSuccessText:sender.currentTitle];
+}
+
+- (IBAction)percentAction:(UIButton *)sender {
+    self.timer = [NSTimer timerWithTimeInterval:0.1 target:self selector:@selector(showPercent) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+    
+}
+- (IBAction)imageAction:(UIButton *)sender {
+    [self showImage:[UIImage imageNamed:@"emj"] text:sender.currentTitle];
+}
+
+
+- (void)showPercent
+{
+    self.percent += 5;
+    [self showProgress:self.percent];
+    
+    if (self.percent == 100) {
+        [self.timer invalidate];
+        self.timer = nil;
+    }
+}
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
